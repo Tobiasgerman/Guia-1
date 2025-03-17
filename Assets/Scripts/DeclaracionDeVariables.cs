cce35ef706127dc04d08da0bd4e96a5ab2a539c5
Tobiasgerman <tobigerman2019@gmail.com>
2025-03-17 10:41:07 -0300

    Primera parte de guia 1 terminada

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class DeclaracionDeVariables : MonoBehaviour
 {
+    int edadUsuario;
+    float sueldoUsuario;
+    string domicilioUsuario;
+    bool estaAprobado;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //inicializo las variables
+        edadUsuario = 30;
+        sueldoUsuario = 2000.00f;
+        domicilioUsuario = "Libertador 1530";
+        estaAprobado = true;
+
+        //Imprimo variables en consola
+        Debug.Log(edadUsuario);
+        Debug.Log(sueldoUsuario);
+        Debug.Log(domicilioUsuario);
+        Debug.Log(estaAprobado);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         
     }
 }
