0edf1e62119909179f0c7b1cbe89c74f0966ee6f
Tobiasgerman <tobigerman2019@gmail.com>
2025-04-03 10:03:17 -0300

    Guia 1 parte 2 ejercicio 1 y 5 terminados

---
+++ Assets/Scripts/DivisionProhibida.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivisionProhibida : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    float resultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num2 == 0)
+        {
+            //instrucciones
+            Debug.Log("No se puede dividir por 0");
+        }
+        else
+        {
+
+            resultado = num1 / (num2 * 1.0f); //cambiando a float 
+            Debug.Log(num1 + "/" + num2 + "=" + resultado);
+        }
+
+       
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
