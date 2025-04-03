91dc5dc953931851a7799ca00caed83d483c0b1b
Tobiasgerman <tobigerman2019@gmail.com>
2025-04-03 10:38:22 -0300

    Ejercicio 10 guia 2 terminado

---
+++ Assets/Scripts/Estacionamiento.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estacionamiento : MonoBehaviour
+{
+    public int hora;
+    bool estaAbierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hora < 0 || hora > 24) // el simbolo "||" es para poner o
+        {
+            Debug.Log("a ingresado una hora incorrecta");
+            return; // el componente return termina la funcion hasta ahi.
+
+        } else if(hora >= 10 && hora < 18)  // el simbolo "&&" es para poner and
+
+        {
+            estaAbierto = true;
+        } else
+
+        {
+            estaAbierto = false;
+        }
+
+        Debug.Log("el estacionamiento esta abierto:" + estaAbierto);
+            
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
